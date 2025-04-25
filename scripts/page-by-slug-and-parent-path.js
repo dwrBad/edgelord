e4141550a9604263583af85032d60cdb1cf57d2a
Dirk Weber <dwr@dasburo.com>
2025-04-25 11:43:20 +0200

    fix: Improve error handling and code formatting in page data fetch handler

--- scripts/page-by-slug-and-parent-path.js
+++ scripts/page-by-slug-and-parent-path.js
@@ -1,15 +1,14 @@
-export default async function handler(request, _context) {
-  const slug = request.params.slug ?? "";
-  const parentPath = request.params.parentPath ?? "/";
-  const PUBLISHER_HOST = 'https://dev.cf.skoda-auto.com'
-  const persistedQuery = `test-site/pageBySlugAndParentPath`
-  const url = `${PUBLISHER_HOST}/graphql/execute.json/${persistedQuery}%3BparentPath%3D%2Fcontent%2Fdam%2Fskoda-auto-dirk%2F${parentPath}%3Bslug%3D${slug};`
-  const res = await fetch(url)
+export default async function handler(request, context) {
+  const slug = request.params.slug ?? '';
+  const parentPath = request.params.parentPath ?? '/';
+  const PUBLISHER_HOST = 'https://dev.cf.skoda-auto.com';
+  const persistedQuery = 'test-site/pageBySlugAndParentPath';
+  const url = `${PUBLISHER_HOST}/graphql/execute.json/${persistedQuery}%3BparentPath%3D%2Fcontent%2Fdam%2Fskoda-auto-dirk%2F${parentPath}%3Bslug%3D${slug};`;
+  const res = await fetch(url);
 
   if (!res.ok) {
-    return {
-      statusCode: res.status,
-      body: JSON.stringify({error: "Failed to fetch from AEM", url}),
+    context.content = {
+      json: JSON.stringify({ error: 'Failed to fetch from AEM', url }),
     };
   }
 
@@ -17,7 +16,6 @@ export default async function handler(request, _context) {
 
   // Add to context for rendering in the HTML
   context.content = {
-    json
+    json,
   };
-
 }
